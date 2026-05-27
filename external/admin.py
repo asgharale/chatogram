5cd8ea90ef4e1a31d3723a50d201230175fcb7b7
asghar ale <asgharale11@gmail.com>
2026-05-27 19:32:42 +0330

    fix .

--- external/admin.py
+++ external/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from .models import SupportChannel
-from .models import PendingDeposit
 
 
 admin.site.register(SupportChannel)
-@admin.register(PendingDeposit)
-class PendingDepositAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "amount_tomans", "coins_to_add", "status", "created_at")
-    list_filter = ("status", "created_at")
-    search_fields = ("id", "user__bale_id", "user__first_name", "user__last_name")
-    readonly_fields = ("user", "amount_tomans", "coins_to_add", "receipt_file_id", "status", "created_at")
